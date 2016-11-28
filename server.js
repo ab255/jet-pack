efd8bb1af1d88188cd74b753cc4d90b88b087e8d
Madison Kerndt <madison.kerndt@gmail.com>
2016-11-28 14:25:18 -0700

    add get route

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const http = require('http');
-const server = http.createServer();
+const express = require('express');
+const app = express();
 
-server.on('request', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World');
-  response.end();
+app.locals.title = 'Jet Pack';
+app.locals.urlName = {
+  url: 'http://google.com/'
+};
+
+// SET
+app.set('port', process.env.PORT || 3000);
+
+// GET
+app.get('/', (request, response) => {
+  response.send('testing');
 });
 
-server.listen(3000, () => {
-  console.log('stuff is working');
+app.get('/api/:id', (request, response) => {
+  const { id } = request.params;
+  const url = app.locals.urlName[id];
+
+  if(!url) {return response.sendStatus(404);}
+
+  response.json({ id, url });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
