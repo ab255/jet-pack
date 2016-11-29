483ab8155bb195410babf523037f791b6841bd3f
Madison Kerndt <madison.kerndt@gmail.com>
2016-11-28 21:21:17 -0700

    reformat info into an array with both data and click count

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.locals.title = 'Jet Pack';
-app.locals.urlNames = [];
+app.locals.urls = [];
 
 // SET
 app.set('port', process.env.PORT || 3000);
 
 // GET
 app.get('/', (request, response) => {
-  response.send({ urls: app.locals.urlNames});
+  response.send({ urls: app.locals.urls});
 });
 
 app.get('/api/:id', (request, response) => {
   const id = request.params;
-  const longUrl = app.locals.urlNames[id];
+  const longUrl = app.locals.urls[id];
 
-  const shortUrl = app.locals.urlNames[id];
+  const shortUrl = app.locals.urls[id];
   if(!url) {return response.sendStatus(404);}
   response.json({ id });
 });
@@ -31,15 +31,17 @@ app.get('/api/:id', (request, response) => {
 app.post('/', (request, response) => {
   const id = shortid.generate();
   const { url } = request.body;
-
+  const date = Date.now();
+  const clicks = 0;
 
   if(!url) {
     return response.sendStatus(404).send({
       error: 'No URL provided'
     });
   }
-  app.locals.urlNames[id] = url;
-  response.status(201).json({ id, url});
+
+  app.locals.urls.push({ id, url, date, clicks });
+  response.status(201).json({ id, url, date, clicks });
 });
 
 app.listen(app.get('port'), () => {
