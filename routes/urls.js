5c55adc2d88fdd53d64909ec40ad9ffd5d4f702d
Madison Kerndt <madison.kerndt@gmail.com>
2016-11-29 13:45:13 -0700

    set up files

---
+++ routes/urls.js
@@ -0,0 +1,7 @@
+var Dinosaur = require('../models/dinosaur');
+var express = require('express');
+var router = express.Router();
+
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
