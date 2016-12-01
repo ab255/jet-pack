b5bc695e0f0b2756f5065305458f3baec471b476
Adam Bernard <ab255@me.com>
2016-12-01 13:11:10 -0700

    nth try

--- server.js
+++ server.js
@@ -9,20 +9,21 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-let dbName = 'urlDB'
-let connectionString = 'https://jetp.herokuapp.com/' + dbName;
+// let dbName = 'urlDB'
+// let connectionString = 'mongodb://https://jetp.herokuapp.com/' + dbName;
 
 const port_number = process.env.PORT || 3000;
 
 if (!module.parent) {
-  let dbName = 'urlDBTest'
-  let connectionString = 'mongodb://localhost:27017/' + dbName;
   app.listen(port_number, () => {
     console.log(`Jet Pack is running on ${port_number}.`);
   });
 }
 
-mongoose.connect(connectionString);
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+  if (error) console.error(error);
+  else console.log('mongo connected');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
